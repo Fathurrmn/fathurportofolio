52d2883c3641be3dbf795e605d738720e962af88
Fathurrmn <144424328+Fathurrmn@users.noreply.github.com>
2025-01-28 16:14:36 +0700

    Upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          beige: '#F5F5DC', // Warna krem dalam kode HEX
+        },
+      },
+    },
+  };
+  
